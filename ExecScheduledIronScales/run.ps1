cf9beb0c52e5b87f9f8a5c1bf38ae5c530ac2a25
Troy Harshberger <tharshberger@vendorassistance.com>
2024-02-21 14:22:45 -0700

    Add force sync queue trigger for IronScales

---
+++ ExecScheduledIronScales/run.ps1
@@ -0,0 +1,17 @@
+param($QueueItem)
+
+try {
+    Write-LogMessage -API "ExecScheduled_IronScales" -tenant "none" -message "Starting IronScales processing." -sev Info
+
+    $Table = Get-CIPPTable -TableName Extensionsconfig
+    $Configuration = (Get-CIPPAzDataTableEntity @Table).config | ConvertFrom-Json -Depth 10
+    if(!$Configuration.IronScales.enabled) {
+        return
+    }
+        
+    Get-IronScalesIncidents -configuration $Configuration.IronScales
+}
+catch {
+    Write-Host $($_.Exception.Message)
+    Write-LogMessage -API "Scheduler_IronScales" -tenant "none" -message "Could not start IronScales processing $($_.Exception.Message)" -sev Error
+}
