4b89c5462c48c5b53dca318c87f2a64e93aca4f9
Troy Harshberger <tharshberger@vendorassistance.com>
2024-02-21 10:54:55 -0700

    Re-add the AT module as git thought it was a submodule...

---
+++ Modules/AutoTaskAPI/1.2.2/Public/Get-AutotaskAPIResource.ps1
@@ -0,0 +1,114 @@
+<#
+.SYNOPSIS
+    Gets a specified resource in the API.
+.DESCRIPTION
+    Gets a specified resource in the API. retrieves data based either on ID or specific JSON query.
+.EXAMPLE
+    PS C:\>  Get-AutotaskAPIResource -resource Companies -id 1234 -verbose
+    Gets the company with ID 1234
+
+    Get-AutotaskAPIResource -resource Companies -SearchQuery "{filter='active -eq True'}"
+    Gets all companies with the filter "Active = true"
+
+.INPUTS
+    -ID: Search by Autotask ID. Accept pipeline input.
+    -SearchQuery: JSON search filter.
+    -SimpleSearch: a simple search filter, e.g. name eq Lime
+   
+.OUTPUTS
+    none
+.NOTES
+    TODO: Turns out some items have child URLS. figure that out.
+#>
+function Get-AutotaskAPIResource {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ParameterSetName = 'ID', Mandatory = $true)]
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [String]$ID,
+        [Parameter(ParameterSetName = 'ID', Mandatory = $false)]
+        [String]$ChildID,
+        [Parameter(ParameterSetName = 'SearchQuery', Mandatory = $true)]
+        [String]$SearchQuery,
+        [Parameter(ParameterSetName = 'SimpleSearch', Mandatory = $true)]
+        [String]$SimpleSearch
+    )
+    DynamicParam {
+        $Script:GetParameter
+    }
+    begin {
+        if (!$Script:AutotaskAuthHeader -or !$Script:AutotaskBaseURI) {
+            Write-Warning "You must first run Add-AutotaskAPIAuth before calling any other cmdlets" 
+            break 
+        }
+        $resource = $PSBoundParameters.resource
+        $headers = $Script:AutotaskAuthHeader
+        $Script:Index = $Script:Queries | Group-Object Index -AsHashTable -AsString
+        $ResourceURL = @(($Script:Index[$resource] | Where-Object { $_.Get -eq $resource }))[0]
+        $ResourceURL.name = $ResourceURL.name.replace("/query", "/{PARENTID}") 
+        if ($SimpleSearch) {
+            $SearchOps = $SimpleSearch -split ' '
+            $SearchQuery = ConvertTo-Json @{
+                filter = @(@{
+                        field = $SearchOps[0]
+                        op    = $SearchOps[1]
+                        value = $SearchOps | Select-Object -Skip 2
+                    })
+            } -Compress
+            
+        }
+    }
+
+    process {
+        if ($resource -like "*child*" -and $SearchQuery) { 
+            Write-Warning "You cannot perform a JSON Search on child items. To find child items, use the parent ID."
+            break
+        }
+        if ($ID) {
+            $ResourceURL = ("$($ResourceURL.name)" -replace '{parentid}', "$($ID)") 
+        }
+        if ($ChildID) { 
+            $ResourceURL = ("$($ResourceURL)/$ChildID")
+        }
+        if ($SearchQuery) { 
+            $ResourceURL = ("$($ResourceURL.name)/query?search=$SearchQuery" -replace '{PARENTID}', '')
+        }
+        $SetURI = "$($Script:AutotaskBaseURI)/$($ResourceURL)"
+        try {
+            do {
+                $items = Invoke-RestMethod -Uri $SetURI -Headers $Headers -Method Get
+                $SetURI = $items.PageDetails.NextPageUrl
+                #[System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([datetime]::UtcNow, (get-timezone).id)
+            
+                if ($items.items) { 
+                    foreach ($item in $items.items) {
+
+                        $item
+                    }
+                }
+                if ($items.item) {
+                    foreach ($item in $items.item) {
+
+                        $item
+                    }
+                    
+                }  
+            } while ($null -ne $SetURI)
+        }
+        catch {
+            if ($psversiontable.psversion.major -lt 6) {
+                $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
+                $streamReader.BaseStream.Position = 0
+                if ($streamReader.ReadToEnd() -like '*{*') { $ErrResp = $streamReader.ReadToEnd() | ConvertFrom-Json }
+                $streamReader.Close()
+            }
+            if ($ErrResp.errors) { 
+                Write-Error "API Error: $($ErrResp.errors)" 
+            }
+            else {
+                Write-Error "Connecting to the Autotask API failed. $($_.Exception.Message)"
+            }
+        }
+
+    }
+}
---
+++ Modules/AutoTaskAPI/1.2.2/Public/Set-AutotaskAPIResource.ps1
@@ -0,0 +1,87 @@
+
+<#
+.SYNOPSIS
+    Sets a resource in the API to the supplied object.
+.DESCRIPTION
+ Sets a resource in the API to the supplied object. Uses the Patch method. Each item in the object will be overwritten at the API side. Null values will overwrite with null values.
+.EXAMPLE
+    PS C:\>  Set-AutotaskAPIResource -resource Companies -ID 1234 -Body $body
+    Updates the company with ID 1234 in the Autotask REST API, changing any parameters in $body.
+
+    PS C:\>  $TicketList = Get-AutotaskAPIResource -Resource Tickets -SimpleSearch "title eq Nope!"
+    PS C:\>  $ticketlist | ForEach-Object { $_.status = "12" }
+    PS C:\>  $ticketlist | Set-AutotaskAPIResource -Resource Tickets
+    Closes all tickets with the subject "Nope!".
+
+    Get-AutotaskAPIResource -Resource Companies -SimpleSearch 'Isactive eq true' | ForEach-Object {$_.Webaddress = "www.google.com"; $_} | Set-AutotaskAPIResource -Resource companies
+    A one-liner to change all companies webaddresses to "google.com".
+
+.INPUTS
+    -Resource: Which resource to find. Tab completion is available.
+    -ID: ID of the resource you want to update. Can be passed as a property on the pipeline.
+    -ParentID: For use with 'Child' endpoints, the ID of the parent you are updating.
+    -Body: A PS object containing all of the parameters you want to update. Can be passed by pipeline.
+    
+.OUTPUTS
+    none
+.NOTES
+    Function might be changed at release of new API.
+#>
+function Set-AutotaskAPIResource {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ParameterSetName = 'ParentID', Mandatory = $false)][String]$ParentId,
+        [Parameter(ValueFromPipelineByPropertyName = $true)]$ID,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]$body
+    )
+    DynamicParam {
+        $Script:PatchParameter
+    }
+    begin {
+        if (!$Script:AutotaskAuthHeader -or !$Script:AutotaskBaseURI) {
+            Write-Warning "You must first run Add-AutotaskAPIAuth before calling any other cmdlets" 
+            break 
+        }
+        $resource = $PSBoundParameters.resource
+        $headers = $Script:AutotaskAuthHeader   
+        $ResourceURL = (($Script:Queries | Where-Object { $_.'Patch' -eq $Resource }).Name | Select-Object -first 1) -replace '/query', '' | Select-Object -first 1
+
+        if ($resource -like "*child*" ) {
+            if ( !$ParentId ) {
+                Write-Warning "You must specify a parentId when settings a child resource" 
+                break 
+            }
+            $ResourceURL = $resourceURL -replace '{parentId}', $ParentId
+        }
+    }
+    
+    process {
+        $MyBody = New-Object -TypeName PSObject
+        if ($ID) {
+            $MyBody | Add-Member -NotePropertyMembers @{id=$ID}
+        }
+        $PSBoundParameters.body.PSObject.properties | Where-Object {$null -ne $_.Value} | ForEach-Object {Add-Member -InputObject $MyBody -NotePropertyMembers @{$_.Name=$_.Value}}
+
+        try {
+            # Iterating through the property names above produces an array of n MyBody objects, all the same,
+            # where n is the number of non-null properties.  Grab the first one.
+            $SendingBody = $MyBody[0] | ConvertTo-Json -Depth 10
+            Invoke-RestMethod -Uri "$($Script:AutotaskBaseURI)/$($ResourceURL)" -headers $Headers -Body $SendingBody -Method Patch
+        }
+        catch {
+            if ($psversiontable.psversion.major -lt 6) {
+                $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
+                $streamReader.BaseStream.Position = 0
+                if ($streamReader.ReadToEnd() -like '*{*') { $ErrResp = $streamReader.ReadToEnd() | ConvertFrom-Json }
+                $streamReader.Close()
+            }
+            if ($ErrResp.errors) { 
+                write-error "API Error: $($ErrResp.errors)" 
+            }
+            else {
+                write-error "Connecting to the Autotask API failed. $($_.Exception.Message)"
+            }
+        }
+
+    }
+}
