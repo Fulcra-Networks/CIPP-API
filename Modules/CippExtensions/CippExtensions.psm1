d251c191838034e6ec27556a3e752abd7585d06c
Troy Harshberger <tharshberger@vendorassistance.com>
2024-07-11 07:37:54 -0600

    Move custom extensions into public

--- Modules/CippExtensions/CippExtensions.psm1
+++ Modules/CippExtensions/CippExtensions.psm1
@@ -1,9 +1,6 @@
 $Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -ErrorAction SilentlyContinue)
-$NinjaOne = @(Get-ChildItem -Path $PSScriptRoot\NinjaOne\*.ps1 -ErrorAction SilentlyContinue)
-$IronScales = @(Get-ChildItem -Path $PSScriptRoot\IronScales\*.ps1 -ErrorAction SilentlyContinue)
-$Autotask = @(Get-ChildItem -Path $PSScriptRoot\Autotask\*.ps1 -ErrorAction SilentlyContinue)
-$Functions = $Public + $Private + $NinjaOne + $IronScales + $Autotask
+$Functions = $Public + $Private
 foreach ($import in @($Functions)) {
     try {
         . $import.FullName
