01bdea3045a27b355f832bc63d07c67b095663c2
Troy Harshberger <tharshberger@fulcra.net>
2025-03-19 14:13:29 -0600

    Update grouptag API to new post request structure.

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/Autopilot/Invoke-ExecSetGroupTag.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/Autopilot/Invoke-ExecSetGroupTag.ps1
@@ -12,17 +12,20 @@ Function Invoke-ExecSetGroupTag {
     $APIName = $TriggerMetadata.FunctionName
     Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
     $tenantfilter = $Request.Body.TenantFilter
+
+    if(![string]::IsNullOrEmpty($Request.body.groupId.value)){
     try {
         $body = @{
-            groupTag = $Request.body.input
+            groupTag = $Request.body.groupId.value
         } | ConvertTo-Json
         New-GraphPOSTRequest -uri "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeviceIdentities/$($request.body.Device)/UpdateDeviceProperties" -tenantid $TenantFilter -body $body -method POST
-        $Results = "Successfully assigned group tag $($Request.body.input) to $($Request.body.device) for $($tenantfilter)"
+        $Results = "Successfully assigned group tag $($Request.body.input) to $($Request.body.ID) for $($tenantfilter)"
     } catch {
         $Results = "Could not assign group tag $($Request.body.input) to $($Request.body.device) for $($tenantfilter) Error: $($_.Exception.Message)"
     }
-
     $Results = [pscustomobject]@{'Results' = "$results" }
+    }
+    else{ $Results = [pscustomobject]@{'Results' = "" } }
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
