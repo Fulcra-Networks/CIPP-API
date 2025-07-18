bcbc9025999a010981445cd289d2776e299d1e81
Troy Harshberger <tharshberger@fulcra.net>
2025-07-18 09:06:25 -0600

    Add logging to determine why standard is failing

--- Modules/CIPPCore/Public/Get-CIPPTenantCapabilities.ps1
+++ Modules/CIPPCore/Public/Get-CIPPTenantCapabilities.ps1
@@ -7,10 +7,14 @@ function Get-CIPPTenantCapabilities {
         $Headers
     )
 
+
+    Write-LogMessage -API $APIName -tenant $TenantFilter -message "Getting Tenant Capabilities: $TenantFilter)" -sev Info
+
     $Org = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/subscribedSkus' -tenantid $TenantFilter
     $Plans = $Org.servicePlans | Where-Object { $_.provisioningStatus -eq 'Success' } | Sort-Object -Property serviceplanName -Unique | Select-Object servicePlanName, provisioningStatus
     $Results = @{}
     foreach ($Plan in $Plans) {
+        Write-LogMessage -API $APIName -tenant $TenantFilter -message "Tenant Capability: $($Plan.servicePlanName)-$($Plan.provisioningStatus))" -sev Info
         $Results."$($Plan.servicePlanName)" = $Plan.provisioningStatus -eq 'Success'
     }
     [PSCustomObject]$Results
